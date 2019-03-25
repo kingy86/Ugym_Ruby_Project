365fc8d19473212f83849e63acaf8003f372f454
Mark <nirvanafoo86@hotmail.com>
2019-03-25 10:56:07 +0000

    Added new table register

--- db/ugym.sql
+++ db/ugym.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS register;
 DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS members;
+
 
 CREATE TABLE sessions(
   id SERIAL8 PRIMARY KEY,
@@ -14,4 +16,10 @@ CREATE TABLE members(
   last_name VARCHAR(255),
   age INT4,
   fitness_level INT4
+);
+
+CREATE TABLE register(
+  id SERIAL8 PRIMARY KEY,
+  member_id INT4 REFERENCES members(id),
+  session_id INT4 REFERENCES sessions(id)
 );
