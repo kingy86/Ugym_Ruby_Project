d2a3b767596de7bceb03626c7d93ed4a5a33206b
Mark <nirvanafoo86@hotmail.com>
2019-03-27 13:42:52 +0000

    Edited sql to delete set null, adding style to erbs

--- db/ugym.sql
+++ db/ugym.sql
@@ -1,4 +1,3 @@
-DROP TABLE register;
 DROP TABLE sessions;
 DROP TABLE members;
 
@@ -16,5 +15,5 @@ CREATE TABLE members(
   last_name VARCHAR(255),
   age INT4,
   fitness_level INT4,
-  session_id INT4 REFERENCES sessions(id)
+  session_id INT4 REFERENCES sessions(id) ON DELETE SET NULL
 );
